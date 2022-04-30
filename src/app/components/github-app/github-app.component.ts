6109344cbb89216ab31da90e3dcfc6682ba6dad2
prachijoshi1609@gmail.com <Prachi1609>
2022-04-30 23:13:04 +0530

    local storage data

--- src/app/components/github-app/github-app.component.ts
+++ src/app/components/github-app/github-app.component.ts
@@ -10,12 +10,43 @@ export class GithubAppComponent implements OnInit {
 
 public githubUserQuery : any;
 public gitHubProfile : any;
-public gitHubRepo : any ;
+  public gitHubRepo!: any[];
+  public loginstore : any;
+  public urlstore : any;
+ profileName :any;
+ profilelink:any;
+ showHistory = false;
+ searchProfile = false;
+
+ arrayhistory! : any[];
+
 
   constructor( private githubService: GithubService) { }
+
+
+
+userProfileHistory(){  
+  this.searchProfile = false;
+  this.showHistory = true;
+  
+  this.loginstore = localStorage.getItem('profiledata');
+  this.urlstore = localStorage.getItem('profilelink');
+  this.profileName = JSON.parse(this.loginstore);
+  this.profilelink = JSON.parse(this.urlstore);
+
+
+
+  console.log(this.profileName);
+  console.log(this.urlstore);
+}
+
   public searchUser(){
+    this.searchProfile = true;
     this.githubService.getProfile(this.githubUserQuery).subscribe((data)=>{
-      this.gitHubProfile = data;
+      this.gitHubProfile = data;  
+      localStorage.setItem("profiledata", JSON.stringify(data.login)); 
+      localStorage.setItem("profilelink", JSON.stringify(data.html_url)); 
+          
     });
 
     //get git repo 
@@ -28,6 +59,7 @@ public gitHubRepo : any ;
   }
 
   ngOnInit(): void {
+   
   }
 
 }
